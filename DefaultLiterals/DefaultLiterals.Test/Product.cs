3aafcd4e230478b11b94619f977cfc31777b90f3
Hernan Guzman <hernandgr@gmail.com>
2018-01-20 14:22:08 -0500

    Added default literals example.

---
+++ DefaultLiterals/DefaultLiterals.Test/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace DefaultLiterals.Test
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
---
+++ DefaultLiterals/DefaultLiterals.Test/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace DefaultLiterals.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OldWay();
+            NewWay();
+
+            Console.ReadKey();
+        }
+
+        private static void OldWay()
+        {
+            int i = default(int);
+            string str = default(string);
+            Product p = default(Product);
+        }
+
+        private static void NewWay()
+        {
+            int i = default;
+            string str = default;
+            Product p = default;
+
+            if (i == default)
+            {
+                Console.WriteLine($"i is default {i}");
+            }
+
+            i = 2;
+
+            if (i != default)
+            {
+                Console.WriteLine($"i is not default now. The new value is {i}");
+            }
+        }
+
+    }
+}
