de9d793791b2b3f0a7cfb64f7a466f25cf19c9f3
Hernan Guzman <hernandgr@gmail.com>
2018-01-20 13:47:41 -0500

    Added ValueTuples example.

---
+++ Tuples/Tuples.Test/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Tuples.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            OldTuples();
+            NewValueTuples();
+
+            Console.ReadKey();
+        }
+
+        private static void OldTuples()
+        {
+            var tuple = new Tuple<string, decimal>("T-Shirt", 100.0m);
+
+            Console.WriteLine($"Product - Name: {tuple.Item1}, Price: {tuple.Item2}");
+        }
+
+        private static void NewValueTuples()
+        {
+            (string name, decimal price) tuple = ("T-Shirt", 100.0m);
+
+            Console.WriteLine($"Product - Name: {tuple.name}, Price: {tuple.price}");
+        }
+    }
+}
