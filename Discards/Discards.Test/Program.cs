8b4b99c7ceffffa68bc31eef76a2ae1c0723fe9b
Hernan Guzman <hernandgr@gmail.com>
2018-01-31 11:34:54 -0500

    Added discards example.

---
+++ Discards/Discards.Test/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Discards.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        static void Discards()
+        {
+            // Out value without discard
+            int price;
+            if (int.TryParse("10", out price))
+            {
+                // ...
+            }
+
+            // Out value with discard
+            if (int.TryParse("10", out int _))
+            {
+                // ...
+            }
+
+            // Discard value in tuples
+            var (_, _, role) = GetEmployeeData();
+            if (role == "Contractor")
+            {
+                // ...
+            }
+        }
+
+        public static (string name, int age, string role) GetEmployeeData()
+        {
+            return ("Juan", 10, "Contractor");
+        }
+    }
+}
